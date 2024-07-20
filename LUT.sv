43dbe4ef3238bd72b075dff58e4556f261b856a4
Tooba12322 <tuubaa1996@gmail.com>
2024-07-20 11:58:10 +0800

    Update LUT.sv

--- LUT.sv
+++ LUT.sv
@@ -1,15 +1,17 @@
-module LUT(F,a0, a1, a2, a3, clk, rst, addr, RAM);
-  output logic F;
+module LUT(RAM, a0, a1, a2, a3, clk, rst);
+  
   input logic a0, a1, a2, a3;
   input logic clk;
   input logic rst;
   output logic RAM[15:0];
-  output logic addr;
+  logic F;
+  logic addr;
+  logic i;
   
   always_comb F = a3 + ((!a3)*(!a2)*(a1)) + ((a2)*(a1)*(!a0));
   always_comb addr = {a3, a2, a1, a0};
   
-  wire i;
+  
   always @(posedge clk or negedge rst) begin
     if (!rst) begin
       for (i=0; i<16; i++) RAM[i] <= '0;
