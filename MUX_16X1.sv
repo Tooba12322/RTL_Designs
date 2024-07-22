cb8920140f8e808883c16f3b72319e289ce69df5
Tooba12322 <tuubaa1996@gmail.com>
2024-07-22 17:47:45 +0800

    Update and rename MUX_16X1_01.sv to MUX_16X1.sv

---
+++ MUX_16X1.sv
@@ -0,0 +1,18 @@
+module MUX_16x1(Y,In,Sel);
+ output logic Y;
+ input [15:0] In;
+ input [3:0] Sel;
+  assign Y = In[Sel]; 
+endmodule
+module MUX_4x1(Y,In,Sel);
+ output logic Y;
+ input [3:0] In;
+ input [1:0] Sel;
+  assign Y = In[Sel]; 
+endmodule
+module MUX_2x1(Y,In,Sel);
+ output logic Y;
+ input [1:0] In;
+ input Sel;
+  assign Y = In[Sel]; 
+endmodule
--- MUX_16X1_01.sv
+++
@@ -1,6 +0,0 @@
-module MUX(Y,In,Sel);
- output logic Y;
- input [15:0] In;
- input [3:0] Sel;
-  assign Y = In[Sel]; 
-endmodule
