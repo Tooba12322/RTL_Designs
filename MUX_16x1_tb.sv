85411bbfeb3182d18c8d96b5d7811bae43b19a8d
Tooba12322 <tuubaa1996@gmail.com>
2024-07-23 11:03:49 +0800

    Update MUX_16x1_tb.sv

--- MUX_16x1_tb.sv
+++ MUX_16x1_tb.sv
@@ -1,24 +1,36 @@
-module MUX_4x1_tb(A,B,C);
+module MUX_16x1_tb(A,B,C);
   input logic A;
-  output logic [3:0] B;
-  output logic [1:0]C;
+  output logic [15:0] B;
+  output logic [3:0]C;
 
-  MUX_4x1 DUT(.Y(A),.In(B),.Sel(C));
+  MUX_16x1 DUT(.Y(A),.In(B),.Sel(C));
 
 initial
  begin
    
-   $monitor ($time,"  A=%b, B=%b, C=%b",A,B,C);
+   $monitor ($time,"  A=%b, B=%h, C=%d",A,B,C);
    
-  #5 B=4'b1001; C=2'b10;
-  #5 B=4'b0111; C=2'b11;
-  #5 B=4'b1101; C=2'b00;
-  #5 B=4'b0110; C=2'b11;
-  #5 B=4'b1001; C=2'b10;
-  #5 B=4'b0101; C=2'b01;
-  #5 B=4'b0001; C=2'b01;
-  #5 B=4'b1111; C=2'b00;
+  #5 B=16'h674F; C=4'd5;
+  #5 C=4'd12;
+  #5 C=4'd8;
+  #5 C=4'd3;
+  #5 B=16'hA017; C=4'd0;
+  #5 C=4'd5;
+  #5 C=4'd10;
+  #5 C=4'd15;
   #5 $finish;
   
  end
 endmodule
+
+Output : 
+0  A=x, B=xxxx, C= x
+                   5  A=0, B=674f, C= 5
+                  10  A=0, B=674f, C=12
+                  15  A=1, B=674f, C= 8
+                  20  A=1, B=674f, C= 3
+                  25  A=1, B=a017, C= 0
+                  30  A=0, B=a017, C= 5
+                  35  A=0, B=a017, C=10
+                  40  A=1, B=a017, C=15
+testbench.sv:21: $finish called at 45 (1s)
