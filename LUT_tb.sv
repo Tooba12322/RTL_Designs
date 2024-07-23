46c74e42094eb6ef8f453db26a5e5a9a78e3f38c
Tooba12322 <tuubaa1996@gmail.com>
2024-07-23 12:14:53 +0800

    Update LUT_tb.sv

--- LUT_tb.sv
+++ LUT_tb.sv
@@ -26,3 +26,23 @@ module LUT_tb(ram, A0, A1, A2, A3, CLK, RST);
  always #2 CLK = !CLK; 
  
 endmodule
+
+Output :
+0  CLK=0, RST=0, A3=0, A2=0, A1=0, A0=0,  RAM=0
+                   2  CLK=1, RST=0, A3=0, A2=0, A1=0, A0=0,  RAM=0
+                   4  CLK=0, RST=0, A3=0, A2=0, A1=0, A0=0,  RAM=0
+                   5  CLK=0, RST=0, A3=0, A2=0, A1=1, A0=1,  RAM=0
+                   6  CLK=1, RST=0, A3=0, A2=0, A1=1, A0=1,  RAM=0
+                   8  CLK=0, RST=0, A3=0, A2=0, A1=1, A0=1,  RAM=0
+                  10  CLK=1, RST=1, A3=0, A2=0, A1=1, A0=1,  RAM=1
+                  12  CLK=0, RST=1, A3=0, A2=0, A1=1, A0=1,  RAM=1
+                  14  CLK=1, RST=1, A3=0, A2=0, A1=1, A0=1,  RAM=1
+                  15  CLK=1, RST=1, A3=1, A2=0, A1=1, A0=0,  RAM=0
+                  16  CLK=0, RST=1, A3=1, A2=0, A1=1, A0=0,  RAM=0
+                  18  CLK=1, RST=1, A3=1, A2=0, A1=1, A0=0,  RAM=1
+                  20  CLK=0, RST=1, A3=0, A2=1, A1=1, A0=1,  RAM=1
+                  22  CLK=1, RST=1, A3=0, A2=1, A1=1, A0=1,  RAM=0
+                  24  CLK=0, RST=1, A3=0, A2=1, A1=1, A0=1,  RAM=0
+testbench.sv:24: $finish called at 25 (1s)
+
+Waveform : https://www.edaplayground.com/w/x/9xZ
