c39a03de1ffa2bb69a23424639c38b2f5d2de428
Tooba12322 <tuubaa1996@gmail.com>
2024-07-23 10:40:15 +0800

    Update MUX_16x1_tb.sv

--- MUX_16x1_tb.sv
+++ MUX_16x1_tb.sv
@@ -1,19 +1,23 @@
-module LUT_tb(A,B,C);
+module MUX_4x1_tb(A,B,C);
   input logic A;
-  output logic [1:0] B;
-  output logic C;
+  output logic [3:0] B;
+  output logic [1:0]C;
 
-  MUX_2x1 DUT(.Y(A),.In(B),.Sel(C));
+  MUX_4x1 DUT(.Y(A),.In(B),.Sel(C));
 
 initial
  begin
    
    $monitor ($time,"  A=%b, B=%b, C=%b",A,B,C);
    
-  #5 B=2'b01; C=1'b0;
-  #5 B=2'b01; C=1'b1;
-  #5 B=2'b10; C=1'b0;
-  #5 B=2'b10; C=1'b1;
+  #5 B=4'b1001; C=2'b10;
+  #5 B=4'b0111; C=2'b11;
+  #5 B=4'b1101; C=2'b00;
+  #5 B=4'b0110; C=2'b11;
+  #5 B=4'b1001; C=2'b10;
+  #5 B=4'b0101; C=2'b01;
+  #5 B=4'b0001; C=2'b01;
+  #5 B=4'b1111; C=2'b00;
   #5 $finish;
   
  end
