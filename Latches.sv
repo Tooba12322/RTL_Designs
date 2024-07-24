1460d8d597bb69ad50850817099937fe63c82721
Tooba12322 <tuubaa1996@gmail.com>
2024-07-24 20:28:55 +0800

    Update Latches.sv

--- Latches.sv
+++ Latches.sv
@@ -5,6 +5,19 @@ module D_latch(Q,I,En);
  always_comb Q = En ? I : Q;
 endmodule
 
-module SR_latch()
-
+// NAND SR-latch 
+module SR_latch_NAND(Q,Qb,S,R);
+ output logic Q,Qb;
+ input logic S,R;
+  always_comb Qb = !(S && Q);
+  always_comb Q = !(R && Qb);
 endmodule
+
+// NOR SR-latch 
+module SR_latch_NAND(Q,Qb,S,R);
+ output logic Q,Qb;
+ input logic S,R;
+ always_comb Qb = !(S || Q);
+ always_comb Q = !(R || Qb);
+endmodule
+
