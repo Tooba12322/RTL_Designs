9ff0cb59438ff2cbda1b06743a5187b3e49cfb51
Tooba12322 <tuubaa1996@gmail.com>
2024-07-25 18:30:25 +0800

    Update serial_adder_4b.sv

--- Serial_Adder/serial_adder_4b.sv
+++ Serial_Adder/serial_adder_4b.sv
@@ -1,1 +1,26 @@
+// 4-bit serial adder
+module serial_adder(Sum,Cy,SI_1,SI_2,load,clk,rst);
+ output logic Sum,Cy;
+ input logic SI_1,SI_2,load,clk,rst;
+  logic [3:0] reg_A,reg_B,stored_sum;
+  logic stored_cy;
+  assign {Cy,Sum} = reg_A[0] + reg_B[0];
+  
+  always @(posedge clk or negedge rst) begin
+    if (!rst) stored_cy <= '0;
+    else stored_cy <= Cy;   
+  end
+  
+  always @(posedge clk or negedge rst) begin
+    if (!rst) reg_A <= '0;
+    else begin
+      reg[3] <= load ? SI_1 : Sum;
+      reg[2] <= reg[3];
+      reg[1] <= reg[2];
+      reg[0] <= reg[1];
+    end
+  end
+endmodule
+
+
 
