a110bbf3b720c08453bd66b6d3b449b42797e186
Tooba12322 <tuubaa1996@gmail.com>
2024-07-26 18:21:45 +0800

    Update comp_2bit.sv

--- Comparator/comp_2bit.sv
+++ Comparator/comp_2bit.sv
@@ -1,1 +1,13 @@
-module comp_2b
+// 2-bit comparator
+module comp_2b(Eq,Gt,Lt,A,B);
+  output logic Eq,Gt,Lt;
+  input logic [1:0]A,B;
+  
+  logic Eq_MSB; 
+  assign Eq_MSB = !(A[1] ^ B[1]);
+  
+  assign  Eq = (A==B) ? '1 : '0;
+  assign  Gt = ((A[1] && !B[1]) || (Eq_MSB && A[0] && !B[0])) ? '1 : '0;
+  assign  Lt = ((!A[1] && B[1]) || (Eq_MSB && !A[0] && B[0])) ? '1 : '0;
+  
+endmodule
