70dbf8850d3879551501a2477f712fc03561a992
Tooba12322 <tuubaa1996@gmail.com>
2024-07-26 19:00:29 +0800

    Update priority_enc.sv

--- Priority_Encoder/priority_enc.sv
+++ Priority_Encoder/priority_enc.sv
@@ -1,1 +1,11 @@
-
+// 4-input priority encoder
+module priority_enc(Out,In0,In1,In2,In3);
+  output logic [1:0]Out;
+  input logic In0, In1, In2, In3;
+  logic Vld;
+  
+  assign Vld = In0 || In1 || In2 || In3;
+  
+  assign Out = Vld ? (In3 ? '1 : (In2 ? 2'b10 : (In1 ? 2'b01 : '0))) : 'x; 
+  
+endmodule
