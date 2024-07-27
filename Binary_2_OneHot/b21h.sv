4da834de4b77c9342afcc608ad2501260db6b054
Tooba12322 <tuubaa1996@gmail.com>
2024-07-27 16:38:12 +0800

    Update b21h.sv

--- Binary_2_OneHot/b21h.sv
+++ Binary_2_OneHot/b21h.sv
@@ -1,1 +1,11 @@
-
+// Binary to one hot encoder using parameters
+module b21h (Bin,One_Hot);
+  
+  parameter Bin_w = 4;
+  parameter O_w = 2**Bin_w;
+  
+  input logic [Bin_w-1:0] Bin;
+  output logic [O_w-1:0] One_Hot;
+  
+  assign One_Hot = 1'b1 << Bin;
+endmodule
