7939cc2beba6692614a7a1b62fd6db9ac3a15508
Tooba12322 <tuubaa1996@gmail.com>
2024-07-27 12:06:29 +0800

    Update edge_detect.sv

--- Edge Detector/edge_detect.sv
+++ Edge Detector/edge_detect.sv
@@ -1,1 +1,17 @@
+// Positive and negative edge detetctor
+
+module edge_detect(P_edge,N_edge,In,clk,rst);
+  input logic  In,clk,rst;
+  output logic  P_edge,N_edge;
+  logic D_In;
+  
+  always @(posedge clk or negedge rst) begin
+    if (!rst) D_In <= '0;
+    else D_In <= In; //Delayed input
+  end
+  
+  always_comb P_edge = In & !D_In;
+  always_comb N_edge = !In & D_In;
+  
+endmodule
 
