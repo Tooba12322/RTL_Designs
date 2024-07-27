ee0a365e3fc033750f831bbc9d4ae6a1a1de009d
Tooba12322 <tuubaa1996@gmail.com>
2024-07-27 14:37:05 +0800

    Update ALU.sv

--- ALU with flags/ALU.sv
+++ ALU with flags/ALU.sv
@@ -14,20 +14,29 @@ module ALU__16b(Sign,Zero,Parity,Overflow,Out,Op,A,B);
   localparam OR  = 3'b100;
   localparam AND = 3'b101;
   localparam XOR = 3'b110;
-  localparam EQ  = 3'b111;
+  localparam POW  = 3'b111;
   
   always_comb begin
     case (Op)
       ADD : {Co,Out} = {1'b0,A} + {1'b0,B};
       SUB : {Bo,Out} = {1'b0,A} - {1'b0,B};
-      SL  :  
-      SR  :
-      OR  :
-      AND :
-      XOR :
-      EQ  :
+      SL  : Out = A << B[1:0];
+      SR  : Out = A >> B[1:0];
+      OR  : Out = A || B;
+      AND : Out = A && B;
+      XOR : Out = A ^ B;
+      POW : Out = A[1:0] ** B[1:0];
     endcase
   end
+      
+    
+  
+  assign Sign = (Op[2:1]=='0) ? Out[15] : 'x; //Only for arithmatic operation
+  assign Zero = ~|Out;
+  assign Parity = ~^Out;
+  assign Overflow = (Op=='0) ? ((A[15] && B[15] && !Out[15]) || (!A[15] && !B[15] && Out[15])) : 'x; //Only for addition
+  
+endmodule
       
     
   
