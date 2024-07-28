7a40ce75df7946a33e448bd4b27bf52b4b0ad8ea
Tooba12322 <tuubaa1996@gmail.com>
2024-07-28 20:05:14 +0800

    Update serial_adder_4b.sv

--- Serial_Adder/serial_adder_4b.sv
+++ Serial_Adder/serial_adder_4b.sv
@@ -14,20 +14,24 @@ module serial_adder(Sum,Cy,SI_1,SI_2,load,clk,rst);
   always @(posedge clk or negedge rst) begin
     if (!rst) reg_A <= '0;
     else begin
-      reg_A[3] <= load ? SI_1 : Sum;
-      reg_A[2] <= reg_A[3];
-      reg_A[1] <= reg_A[2];
-      reg_A[0] <= reg_A[1];
+      //reg_A[3] <= load ? SI_1 : Sum;
+      //reg_A[2] <= reg_A[3];
+      //reg_A[1] <= reg_A[2];
+      //reg_A[0] <= reg_A[1];
+      //Better coding style for same shift reg functionality
+      reg_A <= load ? {SI_1,reg_A[3:1]} : {Sum,reg_A[3:1]}; 
     end
   end
   
   always @(posedge clk or negedge rst) begin
     if (!rst) reg_B <= '0;
     else begin
-      reg_B[3] <= SI_2;
-      reg_B[2] <= reg_B[3];
-      reg_B[1] <= reg_B[2];
-      reg_B[0] <= reg_B[1];
+      //reg_B[3] <= SI_2;
+      //reg_B[2] <= reg_B[3];
+      //reg_B[1] <= reg_B[2];
+      //reg_B[0] <= reg_B[1];
+      //Better coding style for same shift reg functionality
+      reg_B <= {SI_2,reg_B[3:1]}; 
     end
   end
   
@@ -35,15 +39,12 @@ module serial_adder(Sum,Cy,SI_1,SI_2,load,clk,rst);
   always @(posedge clk or negedge rst) begin
     if (!rst) stored_sum <= '0;
     else begin
-      stored_sum[3] <= Sum;
-      stored_sum[2] <= stored_sum[3];
-      stored_sum[1] <= stored_sum[2];
-      stored_sum[0] <= stored_sum[1];
+      //stored_sum[3] <= Sum;
+      //stored_sum[2] <= stored_sum[3];
+      //stored_sum[1] <= stored_sum[2];
+      //stored_sum[0] <= stored_sum[1];
+      //Better coding style for same shift reg functionality
+      stored_sum <= {Sum,stored_sum[3:1]}; 
     end
   end
 endmodule
-
-
-
-
-
