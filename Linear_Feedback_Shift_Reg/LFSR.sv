8032187439f3ae9e3c1f7928563d270e97a20410
Tooba12322 <tuubaa1996@gmail.com>
2024-07-28 23:30:33 +0800

    Update LFSR.sv

--- Linear_Feedback_Shift_Reg/LFSR.sv
+++ Linear_Feedback_Shift_Reg/LFSR.sv
@@ -4,10 +4,18 @@ module lfsr(Q,clk,rst);
   input logic clk,rst;
    
   logic Fo;
-  
+
+  // Non-zero initialization after reset
   assign Fo = !((Q[0] ^ Q[1]) ^ (!Q[2]));
   always @(posedge clk or negedge rst) begin
     if (!rst) Q <= 4'b1000;
     else Q <=  {Fo,Q[3:1]}; 
   end
+  
+  //Also possible with '0 reset value initialization
+  //assign Fo = !(Q[0] ^ Q[1]);
+  //always @(posedge clk or negedge rst) begin
+  //  if (!rst) Q <= 4'b0000;
+  //  else Q <=  {Fo,Q[3:1]}; 
+  //end
 endmodule 
