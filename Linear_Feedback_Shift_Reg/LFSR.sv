53c99a3d5e3019db503fdb5ef16c6f16d56865bc
Tooba12322 <tuubaa1996@gmail.com>
2024-07-28 23:16:40 +0800

    Update LFSR.sv

--- Linear_Feedback_Shift_Reg/LFSR.sv
+++ Linear_Feedback_Shift_Reg/LFSR.sv
@@ -1,1 +1,13 @@
-
+// Linear feedback shift reg
+module lfsr(Q,clk,rst);
+  output logic [3:0]Q;
+  input logic clk,rst;
+   
+  logic Fo;
+  
+  assign Fo = !((Q[0] ^ Q[1]) ^ (!Q[2]));
+  always @(posedge clk or negedge rst) begin
+    if (!rst) Q <= 4'b1000;
+    else Q <=  {Fo,Q[3:1]}; 
+  end
+endmodule 
