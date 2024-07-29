219e8982a648ca2abd904bd99779a12376b5d5a5
Tooba12322 <tuubaa1996@gmail.com>
2024-07-29 10:20:01 +0800

    Update ring_cnt.sv

--- Ring_Counter/ring_cnt.sv
+++ Ring_Counter/ring_cnt.sv
@@ -1,1 +1,14 @@
-
+// Ring counter - Synchronous counter, Fout = Fin / NFF
+module ring_cnt(Q,clk,rst);
+  output logic [3:0]Q;
+  input logic clk,rst;
+   
+  logic Fo;
+  
+  assign Fo = Q[0];
+  always @(posedge clk or negedge rst) begin
+    if (!rst) Q <= 4'b0100;
+    else Q <=  {Fo,Q[3:1]}; 
+  end
+endmodule 
+  
