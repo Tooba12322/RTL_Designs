ec37bc5915f9e5a0e905ded9e047302424924dba
Tooba12322 <tuubaa1996@gmail.com>
2024-07-29 10:26:46 +0800

    Update tw_ring_cnt.sv

--- Johnson_Counter/tw_ring_cnt.sv
+++ Johnson_Counter/tw_ring_cnt.sv
@@ -1,1 +1,15 @@
 
+// Twisted_Ring / Johnson counter - Synchronous counter, Fout = Fin / 2*NFF
+module tw_ring_cnt(Q,clk,rst);
+  output logic [3:0]Q;
+  input logic clk,rst;
+   
+  logic Fo;
+  
+  assign Fo = !Q[0];
+  always @(posedge clk or negedge rst) begin
+    if (!rst) Q <= '0;
+    else Q <=  {Fo,Q[3:1]}; 
+  end
+endmodule 
+  
