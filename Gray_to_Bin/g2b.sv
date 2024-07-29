b4671733a37b19e58fa479f1c0b3ee16df2cc859
Tooba12322 <tuubaa1996@gmail.com>
2024-07-29 20:17:17 +0800

    Update g2b.sv

--- Gray_to_Bin/g2b.sv
+++ Gray_to_Bin/g2b.sv
@@ -1,1 +1,11 @@
-
+module g2b (bin_o,gr_in);
+  input   logic [3:0] gr_in;       
+  output  logic [3:0] bin_o;
+  
+  assign bin_o[3] = gr_in[3];
+  assign bin_o[2] = bin_o[3] ^ gr_in[2];
+  assign bin_o[1] = bin_o[2] ^ gr_in[1];
+  assign bin_o[0] = bin_o[1] ^ gr_in[0];
+  
+  
+endmodule
