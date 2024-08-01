85cb726f0cf54035d0b508fcfefced298f42bc3b
Tooba12322 <tuubaa1996@gmail.com>
2024-08-01 22:54:20 +0800

    Create mod_10_cnt.sv

---
+++ Mod_10_Counter/mod_10_cnt.sv
@@ -0,0 +1,24 @@
+module m10_cnt_tb();
+ 
+  logic clk,rst;
+  logic [3:0] Cnt;
+  
+  M10_cnt DUT1(.*);
+    
+ initial
+  begin
+    $dumpfile("mod_10_counter.vcd");
+    $dumpvars(0,m10_cnt_tb);
+    
+    clk = '0;
+    rst = '0;
+    
+    #3 @(posedge clk) rst = '1;
+    #100 $finish;
+  end
+  
+ always #2 clk = !clk;  
+ 
+endmodule
+
+Waveform : https://www.edaplayground.com/w/x/BsL
