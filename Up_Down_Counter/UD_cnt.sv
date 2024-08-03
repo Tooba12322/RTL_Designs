c9df1d53bda6b7916d6d3bedf92647a45181091b
Tooba12322 <tuubaa1996@gmail.com>
2024-08-03 15:14:25 +0800

    Create UD_cnt.sv

---
+++ Up_Down_Counter/UD_cnt.sv
@@ -0,0 +1,6 @@
+// Implement an 8-bit counter
+  // - Resets to 0
+  // - Increments by 1 every cycle
+  // - Takes the load value if load_i is seen
+  // - Starts to count down after overflow, otherwise count up
+  
