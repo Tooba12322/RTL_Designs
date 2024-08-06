73da0df046cea6153d42cb94c8ee6bca3967e5e6
Tooba12322 <tuubaa1996@gmail.com>
2024-08-06 15:46:38 +0800

    Update pr_detect.sv

--- Parity_Detector/pr_detect.sv
+++ Parity_Detector/pr_detect.sv
@@ -1,17 +1,12 @@
-// Design an parity detector for serial bit stream and generate a single bit output each cycle using Moore FSM,
-// 0 indicates even parity and 1 indicates odd parity as output.
+// Design an parity detector for serial bit stream and generate a single bit output each cycle using Moore FSM, 0 indicates even parity and 1 indicates odd parity as output.
 
 module pr_detect (out,sr_in,clk,rst);
   
   output logic out;
   input logic sr_in,clk,rst;
   
-  //typedef enum logic {EVEN, ODD} statetype;
-  //statetype pr_state,nx_state;
-  // Not working with enum as of now, still debugging
-  
-  parameter EVEN='0, ODD='1;
-  logic pr_state,nx_state;
+  typedef enum {EVEN, ODD} state;
+  state pr_state,nx_state;
   
   always @(posedge clk or negedge rst) begin
     if (!rst) pr_state <= '0;
@@ -23,11 +18,13 @@ module pr_detect (out,sr_in,clk,rst);
       out = '0;
     case (pr_state) 
       EVEN : begin
-               nx_state = (sr_in=='1) ? ODD : EVEN;
+               if (sr_in=='1) nx_state = ODD;
+               else nx_state = EVEN;
                out = '0;
              end
       ODD  : begin
-               nx_state = (sr_in=='1) ? EVEN : ODD;
+               if (sr_in=='1) nx_state = EVEN;
+               else nx_state = ODD;
                out = '1;
              end
     endcase
