4ef2d869da554072f2a62a8b0e6225c155bacc6c
Tooba12322 <tuubaa1996@gmail.com>
2024-08-06 19:31:52 +0800

    Create Seq_Det_tb.sv

---
+++ Sequence_Detector_1/Seq_Det_tb.sv
@@ -0,0 +1,28 @@
+// Waveform : 
+
+module Seq_Det_tb ();
+  
+  logic In,Out;
+  
+  logic clk,rst;
+  
+  Seq_Det  DUT(.*);
+  
+  initial begin
+    $dumpfile("Seq_Det.vcd");
+    $dumpvars(0,Seq_Det_tb);
+    //$monitor($time, " rst=%b, clk=%b,  Light = %s",rst,clk,Out);   
+    clk = '0;
+    rst = '0;
+    In = '0;
+    #9 rst = '1;
+    #3 In = '1;
+    #25 In = '0;
+    #11 In = '1;
+    #25 In = '0;
+    #20 $finish;
+  end
+    
+  always #2 clk = !clk;
+  
+endmodule
