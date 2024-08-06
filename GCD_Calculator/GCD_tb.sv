4e8e2da8640cfb05164afcc9bc493848ec094e0a
Tooba12322 <tuubaa1996@gmail.com>
2024-08-06 18:03:20 +0800

    Create GCD_tb.sv

---
+++ GCD_Calculator/GCD_tb.sv
@@ -0,0 +1,42 @@
+// Waveform : 
+
+module GCD_tb ();
+  
+  logic [15:0] Out;
+  logic Done;
+  
+  logic [15:0] In;
+  logic clk,rst,Start;
+  
+  GCD DUT(.*);
+  
+  initial begin
+    $dumpfile("GCD.vcd");
+    $dumpvars(0,GCD_tb);
+    $monitor($time, " A=%d, B=%d, GCD=%d",DUT.A,DUT.B,Out);
+    
+    clk = '0;
+    rst = '0;
+    Start = '0;
+    In = '0;
+    
+    #9 Start = '1;
+    rst = '1;
+    In = $urandom_range(16'd1,16'd10);
+    
+    #8  In = $urandom_range(16'd1,16'd5);
+    Start = '0;
+    
+    #20 Start = '1;
+    rst = '1;
+    In = $urandom_range(16'd1,16'd6);
+    
+    #8  In = $urandom_range(16'd1,16'd5);
+    //Start = '0;
+    
+    #30 $finish;
+  end
+    
+  always #2 clk = !clk;
+  
+endmodule
