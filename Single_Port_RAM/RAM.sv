cdc99309e21d34a65b365849d79bd04e717b1b9c
Tooba12322 <tuubaa1996@gmail.com>
2024-08-19 17:16:48 +0800

    Update RAM.sv

--- Single_Port_RAM/RAM.sv
+++ Single_Port_RAM/RAM.sv
@@ -1,1 +1,30 @@
 
+// Single port synchronous RAM
+
+module RAM (data,addr,cs,mode,clk,rst);
+  
+  inout logic [7:0] data;
+  input logic cs,mode,clk,rst;
+  input logic [9:0] addr;
+  
+  paramter size = 1024;
+  logic [7:0] RAM [0:size-1];
+  logic [7:0] data_reg;
+  
+  always @(posedge clk or negedge rst) begin
+    if (!rst) begin
+      for (int i=0;i<size;i++) RAM[i] <= '0;
+    end
+    else if (cs && mode) begin
+      for (int i=0;i<size;i++) RAM[i] <= (i==addr) ? data : RAM[i];//write
+    end
+  end
+  
+  always @(posedge clk or negedge rst) begin
+    if (!rst) data_reg <= '0;
+    else if (cs && !mode) data_reg <= RAM[addr];//read
+  end   
+  
+  assign data = (cs && mode) ? data_reg ? 8'bx;
+    
+endmodule
