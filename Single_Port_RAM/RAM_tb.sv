90b697e1e5b865f6233dd523744fa2f45fc4d134
Tooba12322 <tuubaa1996@gmail.com>
2024-08-23 23:46:17 +0800

    Update RAM_tb.sv

--- Single_Port_RAM/RAM_tb.sv
+++ Single_Port_RAM/RAM_tb.sv
@@ -1,10 +1,9 @@
-//Waveform
-
 module RAM_tb ();
-  logic [7:0] data;
+  inout logic [7:0] data;
   logic cs,mode,clk,rst;
   logic [9:0] addr;
-  
+  logic bi_en;
+  logic [7:0] out_data_tb;
   RAM DUT(.*);
   
   initial begin
@@ -14,24 +13,32 @@ module RAM_tb ();
     clk = '0;
     rst = '0;
     mode = '1;
+    bi_en = '1;
+    cs='1;
+    
     #20 @(posedge clk)  rst = '1;
     
     repeat (10) begin 
-      @(posedge clk) data = $urandom_range(0,8'hFF);
-      @(posedge clk) addr = $urandom_range(0,10'h3FF);
+      @(posedge clk) out_data_tb = $urandom_range(0,8'hFF);
+      
+      addr = $urandom_range(0,10'h00F);
     end
     
     
-    @(posedge clk) mode = '0;
-    @(posedge clk) addr = $urandom_range(0,10'h3FF);
+    @(posedge clk) mode = '0; bi_en = '0;
+    addr = $urandom_range(0,10'h3FF);
     
     repeat (10) begin
-      @(posedge clk)    addr = $urandom_range(0,10'h3FF);
+      @(posedge clk)    addr = $urandom_range(0,10'h00F);
     end
-    #50 $finish;
+    
+    
+    $finish;
   end
     
   always #2 clk = !clk;
+  
+  assign data = (bi_en) ? out_data_tb : 8'bx;
   
 endmodule
 
