cb3108e424ebef206be9dca70ff647fb9f90f5b5
Tooba12322 <tuubaa1996@gmail.com>
2024-08-26 11:57:16 +0800

    Update traffic_light.sv

--- Moore_Machine/traffic_light.sv
+++ Moore_Machine/traffic_light.sv
@@ -6,13 +6,13 @@
 
 module traffic_light (Out,clk,rst);
   
-  output logic [6*8:1] Out; // to hold an ASCII value
+  output logic [6*8:1] Out; // to hold an ASCII value of traffic light colour
   input logic clk,rst;
    
   logic [1:0] Cnt;
   logic ld_Cnt;
   
-  typedef enum logic [1:0] {RED = 2'b00,
+  typedef enum logic [1:0] {RED = 2'b00, // traffic light as states
     GREEN = 2'b01,
     YELLOW = 2'b10} state;
     state pr_state,nx_state;
@@ -33,26 +33,26 @@ module traffic_light (Out,clk,rst);
                 nx_state = GREEN;
                 ld_Cnt = '1;
               end
-              Out = "RED";
+              Out = "RED"; // keep RED till cnt becomes zero 
             end
       GREEN : begin
              if (Cnt == '0) begin
                nx_state = YELLOW;
                ld_Cnt = '1;
              end
-             Out = "GREEN";
+             Out = "GREEN"; // keep GREEN till cnt becomes zero 
            end
       YELLOW : begin
               if (Cnt == '0) begin
                 nx_state = RED;
                 ld_Cnt = '1;
               end
-              Out = "YELLOW";
+              Out = "YELLOW"; // keep YELLOW till cnt becomes zero 
            end
       endcase
   end
   
-  always @(posedge clk or negedge rst) begin
+  always @(posedge clk or negedge rst) begin // down counter
     if (!rst || ld_Cnt) Cnt <= '1;
     else Cnt <= Cnt - 2'd1;
   end 
