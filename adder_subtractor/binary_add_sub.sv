d5b4b24bf9cd0173f747ac90c9f5a65e4c79fb38
Tooba12322 <tuubaa1996@gmail.com>
2024-08-26 12:51:13 +0800

    Update binary_add_sub.sv

--- adder_subtractor/binary_add_sub.sv
+++ adder_subtractor/binary_add_sub.sv
@@ -6,7 +6,7 @@ module binary_add_sub(Sum,Co,M,A,B);
  input logic [3:0]A,B;
   logic Cin,c1,c2,c3;
   assign Cin = M;
-  assign {c1,Sum[0]} = A[0] + (B[0] ^ M) + Cin;
+ assign {c1,Sum[0]} = A[0] + (B[0] ^ M) + Cin; //acts as adder when M=0, else subtract using B's 2's complement
   assign {c2,Sum[1]} = A[1] + (B[1] ^ M) + c1;
   assign {c3,Sum[2]} = A[2] + (B[2] ^ M) + c2;
   assign {Co,Sum[3]} = A[3] + (B[3] ^ M) + c3;
