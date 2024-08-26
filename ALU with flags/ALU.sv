ff32c4860dedf0e98ac193b2ac0aca9281f5a89a
Tooba12322 <tuubaa1996@gmail.com>
2024-08-26 11:34:53 +0800

    Update ALU.sv

--- ALU with flags/ALU.sv
+++ ALU with flags/ALU.sv
@@ -1,13 +1,13 @@
 // ALU with flags
 module ALU_16b(Sign,Zero,Parity,Overflow,Out,Op,A,B);
   output logic [15:0] Out;
-  output logic Sign,Zero,Parity,Overflow;
+  output logic Sign,Zero,Parity,Overflow;//flags to be updated
   input logic [15:0] A;
   input logic [15:0] B; 
-  input logic [2:0] Op;
-  logic Co,Bo;
+  input logic [2:0] Op;//ALU func selected depending on Op
+  logic Co,Bo;// Carry and borrow
   
-  localparam ADD = 3'b000;
+  localparam ADD = 3'b000;// ALU functions
   localparam SUB = 3'b001;
   localparam SL  = 3'b010;
   localparam SR  = 3'b011;
@@ -18,7 +18,7 @@ module ALU_16b(Sign,Zero,Parity,Overflow,Out,Op,A,B);
   
   always @(A,B,Op) begin
     case (Op)
-      ADD : {Co,Out} = {1'b0,A} + {1'b0,B};
+      ADD : {Co,Out} = {1'b0,A} + {1'b0,B}; // Case depending on Op
       SUB : {Bo,Out} = {1'b0,A} - {1'b0,B};
       SL  : Out = A << B[1:0];
       SR  : Out = A >> B[1:0];
