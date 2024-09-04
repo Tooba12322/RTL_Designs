95211048e20c54d50922bb385d485335381b1a54
Tooba12322 <tuubaa1996@gmail.com>
2024-09-04 15:46:29 +0800

    Update MIPS32_tb3.sv

--- MIPS32_Processor/MIPS32_tb3.sv
+++ MIPS32_Processor/MIPS32_tb3.sv
@@ -1,4 +1,4 @@
-//Waveform : https://www.edaplayground.com/w/x/Be4
+//Waveform : 
 
 // Write the MIPS32 assembly language program and SV testbench 
 // Compute the factorial of a number stored at memory location 200
@@ -23,7 +23,7 @@ module MIPS32_tb3();
   begin
     $dumpfile("MIPS32.vcd");
     $dumpvars(0,MIPS32_tb3);
-    $monitor ($time,"  rst=%b, clk_1=%b, clk_2=%b, R1=%d, R2=%d, R3=%d, R4=%d, R5=%d  ",rst,clk_1,clk_2,DUT.Reg[1],DUT.Reg[2],DUT.Reg[3],DUT.Reg[4],DUT.Reg[5]);
+    $monitor ($time,"  rst=%b, clk_1=%b, clk_2=%b, R10=%d, R2=%d, R3=%d, D_Mem[200]=%d, D_Mem[198]=%d  ",rst,clk_1,clk_2,DUT.Reg[10],DUT.Reg[2],DUT.Reg[3],DUT.D_Mem[200],DUT.D_Mem[198]);
   end
   
   initial begin
@@ -42,20 +42,19 @@ module MIPS32_tb3();
     end
 
     for (int i='0; i<512; i++) begin
-      DUT.D_Mem[i] = i;
+      DUT.D_Mem[i] = 5;
     end
-    
-    DUT.I_Mem[0] = 32'h2801000A;
-    DUT.I_Mem[1] = 32'h28020014;
-    DUT.I_Mem[2] = 32'h28030019;
-    DUT.I_Mem[3] = 32'h0CE77800;
-    DUT.I_Mem[4] = 32'h0CE77800;
-    DUT.I_Mem[5] = 32'h00222000;
-    DUT.I_Mem[6] = 32'h0CE77800;
-    DUT.I_Mem[7] = 32'h0CE77800;
-    DUT.I_Mem[8] = 32'h0CE77800;
-    DUT.I_Mem[9] = 32'h00832800;
-    DUT.I_Mem[10] = 32'hFC000000;
+   
+    DUT.I_Mem[0] = 32'h280A00C8;
+    DUT.I_Mem[1] = 32'h28020001;
+    DUT.I_Mem[2] = 32'h0E94A000;
+    DUT.I_Mem[3] = 32'h21430000;
+    DUT.I_Mem[4] = 32'h0E94A000;
+    DUT.I_Mem[5] = 32'h14431000;
+    DUT.I_Mem[6] = 32'h2C630001;
+    DUT.I_Mem[7] = 32'h00832800;
+    DUT.I_Mem[8] = 32'h2543FFFE;
+    DUT.I_Mem[9] = 32'hFC000000;
 
   end
   
@@ -63,4 +62,3 @@ module MIPS32_tb3();
     always   #2 clk_2 = !clk_2; 
    
 endmodule
-
