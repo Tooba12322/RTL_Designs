8dae04afa3da3e14f44a58651ce52ce23d1e6ba8
Tooba12322 <tuubaa1996@gmail.com>
2024-09-10 14:23:42 +0800

    Create dual_gr_cnt.sv

---
+++ Dual_Gray_Counter/dual_gr_cnt.sv
@@ -0,0 +1,5 @@
+// Design a gray code counter that generates all 4-bit and 3-bit gray counts.
+// n-bit and (n-1) bit gray counter, the (n-1)-bit Gray code is simply generated 
+// by doing an exclusive-or operation on the two MSBs of the n-bit Gray code
+// to generate the MSB for the (n-1)-bit Gray code.
+// This is combined with the (n-2) LSBs of the n-bit Gray code counter to form the (n-1)-bit Gray code counter.
