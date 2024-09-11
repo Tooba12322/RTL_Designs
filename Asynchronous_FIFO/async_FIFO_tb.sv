7831281c91c795f775955dddf9c28d40c68980c1
Tooba12322 <tuubaa1996@gmail.com>
2024-09-11 16:01:56 +0800

    Update async_FIFO_tb.sv

--- Asynchronous_FIFO/async_FIFO_tb.sv
+++ Asynchronous_FIFO/async_FIFO_tb.sv
@@ -1,9 +1,12 @@
 //Waveform (w_clk>r_clk) : https://www.edaplayground.com/w/x/7NW
 //Waveform (r_clk>w_clk) : https://www.edaplayground.com/w/x/4ur
+// When write domain clk is faster than read domain clk, empty signal de-assertion takes more time, as it works on read clk
+// Similarly, When read domain clk is faster than write domain clk, full signal de-assertion takes more time, as it works on write clk
+// As there is no delay with full/empty assertion, late de-assertion should not create any problem.
 
 module async_FIFO_tb();
   
-  parameter depth = 8;
+  parameter depth = 8; // 8-deep FIFO
   parameter width = 8;
   parameter addr  = $clog2(depth);
   
@@ -18,17 +21,22 @@ module async_FIFO_tb();
     $dumpfile("async_FIFO.vcd");
     $dumpvars(0,async_FIFO_tb);
     $monitor ($time,"  r_rst=%b, r_clk=%b, w_rst=%b, w_clk=%b,  wr=%b, rd=%b, FIFO[0]=%h, FIFO[1]=%h, FIFO[6]=%h, FIFO[7]=%h ",r_rst,r_clk,w_rst,w_clk,wr,rd,DUT.FIFO[0],DUT.FIFO[1],DUT.FIFO[6],DUT.FIFO[7]);
-    
+
+    //Initialization
+    // read domain clk,rst
     r_clk = '0;
     r_rst = '0;
+    // write domain clk,rst
     w_clk = '0;
     w_rst = '0;
+    
     rd  = '0;
     wr  = '0;
-        
+
+    //Synchronous de-assertion of read,write domain rst
     #13  w_rst = '1;
     r_rst = '1;
-    #2 @(posedge w_clk) wr = '1;
+    #2 @(posedge w_clk) wr = '1; // writing to FIFO
     wr_data = $urandom_range(0,8'hFF);
     
     repeat (8) begin 
@@ -41,7 +49,7 @@ module async_FIFO_tb();
       @(posedge w_clk) wr_data = $urandom_range(0,8'hFF); 
     end
     
-    #2 rd = '1;
+    #2 rd = '1;//reading from FIFO
     
     #40 rd = '0;
     
@@ -59,7 +67,7 @@ module async_FIFO_tb();
     
     #10 $finish;
   end
-  // w_clk > r_clk 
+  // w_clk > r_clk ,clock generation
   always #2 w_clk = !w_clk;
   
   always #4 r_clk = !r_clk;
