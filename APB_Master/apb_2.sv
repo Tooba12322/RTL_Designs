aea956b58ab521d85f8c65d31b75911699e52b51
Tooba12322 <tuubaa1996@gmail.com>
2024-09-11 16:27:43 +0800

    Create apb_2.sv

---
+++ APB_Master/apb_2.sv
@@ -0,0 +1,26 @@
+// APB Master
+
+// TB should drive a cmd_i input decoded as:
+//  - 2'b00 - No-op
+//  - 2'b01 - Read from address 0xDEAD_CAFE
+//  - 2'b10 - Increment the previously read data and store it to 0xDEAD_CAFE
+//  - 2'b11 - Invalid/Not possible 
+
+module apb_2 (
+  input       wire        clk,
+  input       wire        reset,
+
+  input       wire[1:0]   cmd_i,
+
+  output      wire        psel_o,
+  output      wire        penable_o,
+  output      wire[31:0]  paddr_o,
+  output      wire        pwrite_o,
+  output      wire[31:0]  pwdata_o,
+  input       wire        pready_i,
+  input       wire[31:0]  prdata_i
+);
+
+ 
+
+endmodule
