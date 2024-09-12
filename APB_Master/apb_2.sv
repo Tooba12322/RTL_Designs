7a380a47f7378a9d1d17515d7135e701869afb2a
Tooba12322 <tuubaa1996@gmail.com>
2024-09-12 15:04:46 +0800

    Update apb_2.sv

--- APB_Master/apb_2.sv
+++ APB_Master/apb_2.sv
@@ -7,20 +7,65 @@
 //  - 2'b11 - Invalid/Not possible 
 
 module apb_2 (
-  input       wire        clk,
-  input       wire        reset,
+  input       logic        clk,
+  input       logic        rst,
 
-  input       wire[1:0]   cmd_i,
+  input       logic[1:0]   cmd_i,
 
-  output      wire        psel_o,
-  output      wire        penable_o,
-  output      wire[31:0]  paddr_o,
-  output      wire        pwrite_o,
-  output      wire[31:0]  pwdata_o,
-  input       wire        pready_i,
-  input       wire[31:0]  prdata_i
+  output      logic        psel_o,
+  output      logic        penable_o,
+  output      logic[31:0]  paddr_o,
+  output      logic        pwrite_o,
+  output      logic[31:0]  pwdata_o,
+  input       logic        pready_i,
+  input       logic[31:0]  prdata_i
 );
-
- 
-
+  
+  
+  logic[1:0]   cmd;
+  logic        psel;
+  logic        penable;
+  logic[31:0]  paddr, paddr_nxt;
+  logic        pwrite, pwrite_nxt;
+  logic[31:0]  pwdata, pwdata_nxt;
+  logic[31:0]  prdata;
+  
+  
+  typedef enum logic [1:0] {IDLE = 2'b00, 
+    SETUP = 2'b01,
+    ACCESS = 2'b10} state;
+    state pr_state,nx_state;
+  
+  always @(posedge clk or negedge rst) begin
+    if (!rst) pr_state <= IDLE;
+    else pr_state <= nx_state;
+  end   
+  
+  always_comb begin
+      nx_state = pr_state;
+          
+    case (pr_state) 
+      IDLE : begin
+              if (Cnt == '0) begin
+                nx_state = SETUP;
+                ld_Cnt = '1;
+              end
+              Out = "RED"; // keep RED till cnt becomes zero 
+            end
+      SETUP : begin
+             if (Cnt == '0) begin
+               nx_state = YELLOW;
+               ld_Cnt = '1;
+             end
+             Out = "GREEN"; // keep GREEN till cnt becomes zero 
+           end
+      ACCESS : begin
+              if (Cnt == '0) begin
+                nx_state = RED;
+                ld_Cnt = '1;
+              end
+              Out = "YELLOW"; // keep YELLOW till cnt becomes zero 
+           end
+      endcase
+  end
 endmodule
