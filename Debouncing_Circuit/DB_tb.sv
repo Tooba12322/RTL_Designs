08c5b0c867626e1acf26b1ee7805e54212235dc4
Tooba12322 <tuubaa1996@gmail.com>
2024-09-25 16:04:38 +0800

    Update DB_tb.sv

--- Debouncing_Circuit/DB_tb.sv
+++ Debouncing_Circuit/DB_tb.sv
@@ -1,1 +1,36 @@
+// Waveform : 
 
+module DB_tb ();
+  
+  logic sw,db;
+  
+  logic clk,rst;
+  
+  DB  DUT(.*);
+  
+  initial begin
+    $dumpfile("DB.vcd");
+    $dumpvars(0,DB_tb);
+    //$monitor($time, " rst=%b, clk=%b, Out = %b",rst,clk,Out);   
+    clk = '0;
+    rst = '0;
+    sw = '0;
+    #9 rst = '1;
+    #3 sw = '1;
+    #25 sw = '0;
+    #11 sw = '1;
+    #25 sw = '0;
+    #5  sw ='1;
+    #500 sw='0;
+    #3 sw = '1;
+    #25 sw = '0;
+    #11 sw = '1;
+    #25 sw = '0;
+    #5  sw ='0;
+    #500 sw='1;
+    #20 $finish;
+  end
+    
+  always #2 clk = !clk;
+  
+endmodule
