dafeb9ff908bbaf57d8bfe51c4e10576dd1ec831
Tooba12322 <tuubaa1996@gmail.com>
2024-09-25 17:24:25 +0800

    Update DB_tb.sv

--- Debouncing_Circuit/DB_tb.sv
+++ Debouncing_Circuit/DB_tb.sv
@@ -11,26 +11,33 @@ module DB_tb ();
   initial begin
     $dumpfile("DB.vcd");
     $dumpvars(0,DB_tb);
-    //$monitor($time, " rst=%b, clk=%b, Out = %b",rst,clk,Out);   
+    //$monitor($time, " rst=%b, clk=%b, db = %b",rst,clk,db);   
     clk = '0;
     rst = '0;
     sw = '0;
     #9 rst = '1;
-    #3 sw = '1;
-    #25 sw = '0;
-    #11 sw = '1;
-    #25 sw = '0;
-    #5  sw ='1;
-    #500 sw='0;
-    #3 sw = '1;
-    #25 sw = '0;
-    #11 sw = '1;
-    #25 sw = '0;
-    #5  sw ='0;
-    #500 sw='1;
-    #20 $finish;
+    #100 sw = '1;
+    for (int i='0;i<12;i++) begin
+      sw = !sw;
+      #10;
+    end
+    sw = '1;
+    #3000 sw='0;
+    for (int i='0;i<9;i++) begin
+      sw = !sw;
+      #6;
+    end
+    sw = '0;
+    #4000 sw='1;
+    for (int i='0;i<6;i++) begin
+      sw = !sw;
+      #13;
+    end
+    sw = '1;
+    #4000 sw='0;
+    #550 $finish;
   end
     
-  always #2 clk = !clk;
+  always #5 clk = !clk;
   
 endmodule
