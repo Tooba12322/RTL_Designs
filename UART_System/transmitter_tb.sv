e86ae0a34d914ecaceca56cd2d967df37c41b10e
Tooba12322 <tuubaa1996@gmail.com>
2024-09-30 16:42:40 +0800

    Update transmitter_tb.sv

--- UART_System/transmitter_tb.sv
+++ UART_System/transmitter_tb.sv
@@ -2,9 +2,9 @@
 `timescale 1ns/1ps
 module transmitter_tb();
  
-  logic tick;
-  logic [10:0] br_div;
-  logic clk,rst;
+  logic tx,tx_done; 
+  logic tick,tx_start,clk,rst;
+  logic [2:0] din;
   
   transmitter DUT(.*);
     
@@ -13,12 +13,23 @@ module transmitter_tb();
     $dumpfile("transmitter.vcd");
     $dumpvars(0,transmitter_tb);
     
-    clk = '0;
-    rst = '0;
+    clk      = '0;
+    rst      = '0;
+    tx_start = '0;
+    tick     = '0;
+    din      = '0;
     
     #7 @(posedge clk) rst = '1;
-    br_div = 11'd650;
-    #100000 $finish;
+
+   #7 @(posedge clk) tx_start = '1; din = 3'd5;
+   #3 @(posedge clk) tx_start = '0;
+   
+   for (int i=0;i<100;i++) begin
+    #3 @(posedge clk) tick = '1;
+    #3 @(posedge clk) tick = '0;
+    #165;
+   end
+   #10 $finish;
   end
   
  always #5 clk = !clk;  
