23d341e150724fe6e43e64c9d726162bc3b4ffb5
Tooba12322 <tuubaa1996@gmail.com>
2024-10-01 13:43:22 +0800

    Update UART_top.sv

--- UART_System/UART_top.sv
+++ UART_System/UART_top.sv
@@ -3,16 +3,14 @@
 // Source : https://www.youtube.com/watch?v=Sz6_yH6XGWA&list=PL-iIOnHwN7NUpkOWAQ9Fc7MMddai9vHvN&index=69
 `timescale 1ns/1ps
 
-module UART_top (parity_o,dout,rx_done,tx_done,din,tx_start,clk,rst);
-
-  
-  output logic tx,tx_done; 
-  input logic tick,tx_start,clk,rst;
-  input logic [2:0] din;
-  output logic rx_done,parity_o; // received all bits , assert parity as separate output
+module UART_top (parity_o,dout,rx_done,tx_done,din,tx_start,br_div,clk,rst);
+ 
+  output logic rx_done,tx_done; 
+  output logic parity_o; // received all bits , assert parity as separate output
   output logic [2:0] dout;
-  input logic rx,tick,clk,rst;
-
+  input logic rx,tx_start,clk,rst;
+  input logic [2:0] din;
+  
   logic [3:0] tick_cnt,tick_cnt_nxt; //counter to calculate number of ticks
   logic [1:0] dbits_cnt,dbits_cnt_nxt; //counter to calculate number of data bits received
   logic [2:0] data_out, data_out_nxt;
