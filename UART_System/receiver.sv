027c725f5ec718437523f2a81f26e4c3722193e2
Tooba12322 <tuubaa1996@gmail.com>
2024-10-01 12:25:55 +0800

    Update receiver.sv

--- UART_System/receiver.sv
+++ UART_System/receiver.sv
@@ -2,36 +2,33 @@
 // Source : https://www.youtube.com/watch?v=8M6rEm1EG4w&list=PL-iIOnHwN7NUpkOWAQ9Fc7MMddai9vHvN&index=73
 `timescale 1ns/1ps
 
-module receiver (dout,rx_done,rx,tick,clk,rst);
+module receiver (parity,dout,rx_done,rx,tick,clk,rst);
   
-  output logic rx_done;
+  output logic rx_done,parity;
   output logic [2:0] dout;
   input logic rx,tick,clk,rst;
 
   logic [3:0] tick_cnt,tick_cnt_nxt; //counter to calculate number of ticks
   logic [1:0] dbits_cnt,dbits_cnt_nxt; //counter to calculate number of data bits transmitted
-  logic [2:0] data_in, data_nxt;
-  logic tx_reg,tx_nxt;
-  logic parity_reg,parity_nxt;
-
+  logic [2:0] data_out, data_out_nxt;
+  logic parity_reg, parity_nxt;
+  
   parameter DBITS = 3;
   typedef enum logic [2:0] {idle = 3'd0,start = 3'd1,data = 3'd2,parity = 3'd3,stop = 3'd4} state;
   state pr_state,nx_state;
 
   always @(posedge clk or negedge rst) begin
     if (!rst) begin
-      dbits_cnt <= '0;
-      data_in   <= '0;
-      tx_reg    <= '1;
-      tick_cnt  <= '0;
-      parity_reg<= '0;
+      dbits_cnt  <= '0;
+      data_out   <= '0;
+      tick_cnt   <= '0;
+      parity_reg <= '0;
     end
     else begin
-      dbits_cnt <= dbits_cnt_nxt;
-      data_in   <= data_nxt;
-      tx_reg    <= tx_nxt;
-      tick_cnt  <= tick_cnt_nxt;
-      parity_reg<= parity_nxt;
+      dbits_cnt  <= dbits_cnt_nxt;
+      data_out   <= data_out_nxt;
+      tick_cnt   <= tick_cnt_nxt;
+      parity_reg <= parity_nxt;
     end
   end
   
@@ -43,19 +40,14 @@ module receiver (dout,rx_done,rx,tick,clk,rst);
   always @(*) begin
       nx_state      = pr_state;
       dbits_cnt_nxt = dbits_cnt;
-      data_nxt      = data_in;
-      tx_nxt        = tx_reg;
+      data_out_nxt  = data_out;
       tick_cnt_nxt  = tick_cnt;
-      tx_done       = '0;
+      rx_done       = '0;
       parity_nxt    = parity_reg;
     
     case (pr_state) 
       idle    : begin
-                  if (tx_start == '1) begin
-                    nx_state   = start;
-                    data_nxt   = din;
-                    parity_nxt = ^din;
-                  end
+                  if (rx == '0) nx_state   = start;   
                 end
         
       start   : begin
