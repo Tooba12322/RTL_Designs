21a28fe5f6ebcc83a854d8eab847527e72f30edb
Tooba12322 <tuubaa1996@gmail.com>
2024-10-01 12:39:22 +0800

    Update receiver.sv

--- UART_System/receiver.sv
+++ UART_System/receiver.sv
@@ -51,9 +51,8 @@ module receiver (parity,dout,rx_done,rx,tick,clk,rst);
                 end
         
       start   : begin
-                  tx_nxt = '0;
                   if (tick=='1) begin
-                    if (tick_cnt ==  4'd15) begin
+                    if (tick_cnt ==  4'd7) begin
                       nx_state = data;
                       tick_cnt_nxt = '0;
                     end
@@ -62,11 +61,10 @@ module receiver (parity,dout,rx_done,rx,tick,clk,rst);
                 end
         
       data    : begin
-                  tx_nxt = data_in[0];
                   if (tick=='1) begin
                     if (tick_cnt ==  4'd15) begin
                       tick_cnt_nxt = '0;
-                      data_nxt     = data_in >> 1;
+                      data_out_nxt = {rx,data_out[2:1]};
                       if (dbits_cnt == DBITS - 1) nx_state = parity;
                       else dbits_cnt_nxt = dbits_cnt + 2'd1;
                     end
@@ -75,23 +73,22 @@ module receiver (parity,dout,rx_done,rx,tick,clk,rst);
                 end
         
       parity  : begin
-                  tx_nxt = parity_reg;
                   if (tick=='1) begin
                     if (tick_cnt ==  4'd15) begin
                       nx_state = stop;
                       tick_cnt_nxt = '0;
+                      parity_nxt = rx;
                     end
                     else tick_cnt_nxt = tick_cnt + 4'd1;
                   end
                  end
       
       stop    : begin
-                  tx_nxt = '1;
                   if (tick=='1) begin
                     if (tick_cnt ==  4'd15) begin
                       nx_state = idle;
                       tick_cnt_nxt = '0;
-                      tx_done = '1;
+                      rx_done = '1;
                     end
                     else tick_cnt_nxt = tick_cnt + 4'd1;
                   end
@@ -99,7 +96,8 @@ module receiver (parity,dout,rx_done,rx,tick,clk,rst);
     endcase
   end
   
-  assign tx = tx_reg;
+  assign parity = parity_reg;
+  assign dout   = data_out;
   
 endmodule
  
