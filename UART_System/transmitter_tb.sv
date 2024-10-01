931ec6be05a67cd0e690b89fa3282c7210690bbc
Tooba12322 <tuubaa1996@gmail.com>
2024-10-01 12:03:17 +0800

    Update transmitter_tb.sv

--- UART_System/transmitter_tb.sv
+++ UART_System/transmitter_tb.sv
@@ -1,4 +1,5 @@
-// Waveform : 
+// Waveform : https://www.edaplayground.com/w/x/GyZ
+
 `timescale 1ns/1ps
 module transmitter_tb();
  
@@ -21,14 +22,14 @@ module transmitter_tb();
     
     #7 @(posedge clk) rst = '1;
 
-   #7 @(posedge clk) tx_start = '1; din = 3'd5;
-   #3 @(posedge clk) tx_start = '0;
+    #7 @(posedge clk) tx_start = '1; din = 3'd5;
+    #3 @(posedge clk) tx_start = '0;
    
-   for (int i=0;i<100;i++) begin
-    #3 @(posedge clk) tick = '1;
-    #3 @(posedge clk) tick = '0;
-    #165;
-   end
+    for (int i=0;i<100;i++) begin
+      #3 @(posedge clk) tick = '1;
+      #3 @(posedge clk) tick = '0;
+      #162;
+    end
    #10 $finish;
   end
   
