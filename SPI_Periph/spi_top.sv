24b0564c1dcd28c5ab362b11e57c17333d185c30
Tooba12322 <tuubaa1996@gmail.com>
2024-10-08 22:25:33 +0800

    Update spi_top.sv

--- SPI_Periph/spi_top.sv
+++ SPI_Periph/spi_top.sv
@@ -16,7 +16,7 @@ module spi_m (dout,ready,sclk,mosi,done,miso,cpol,cpha,din,dvsr,start,clk,rst);
   logic [2:0] dbits_cnt,dbits_cnt_nxt; //counter to calculate number of data bits transmitted
   logic [7:0] data_in, din_nxt; // flop for storing data thai is serially available on miso
   logic [7:0] dout_reg,dout_nxt; // flop for storing data to be serially transferred on mosi
-  logic iclk,sclk_nxt,sclk_reg;
+  logic iclk,sclk_nxt,sclk_reg,done_nxt;
 
   parameter DBITS = 8; //total number of data bits to be communicated
   
@@ -30,6 +30,7 @@ module spi_m (dout,ready,sclk,mosi,done,miso,cpol,cpha,din,dvsr,start,clk,rst);
       dout_reg  <= '0;
       cnt       <= '0;
       sclk_reg  <= '0;
+      done      <= '0;
     end
     else begin
       dbits_cnt <= dbits_cnt_nxt;
@@ -37,6 +38,7 @@ module spi_m (dout,ready,sclk,mosi,done,miso,cpol,cpha,din,dvsr,start,clk,rst);
       dout_reg  <= dout_nxt;
       cnt       <= cnt_nxt;
       sclk_reg  <= sclk_nxt;
+      done      <= done_nxt;
     end
   end
   
@@ -51,7 +53,7 @@ module spi_m (dout,ready,sclk,mosi,done,miso,cpol,cpha,din,dvsr,start,clk,rst);
       din_nxt       = data_in;
       dout_nxt      = dout_reg;
       cnt_nxt       = cnt;
-      done          = '0; // assert done at the end for one cycle
+      done_nxt      = '0; // assert done at the end for one cycle
       ready         = '0; // during data transfer ready=0
           
     case (pr_state) 
@@ -86,8 +88,9 @@ module spi_m (dout,ready,sclk,mosi,done,miso,cpol,cpha,din,dvsr,start,clk,rst);
                        if (dbits_cnt == DBITS - 1) begin
                          nx_state = idle;
                          cnt_nxt  = '0;
-                         done     = '1;
+                         done_nxt = '1;
                          dbits_cnt_nxt = '0;
+                         din_nxt  = {data_in[6:0],miso};// for last bit to be captured
                        end
                        else begin
                          dbits_cnt_nxt = dbits_cnt + 3'd1;
@@ -156,34 +159,25 @@ module spi_s (miso,dout,ready,sclk,mosi,done,din,cpha,cpol,rst);
     case (pr_state) 
       idle      : begin 
                    if (ready == '0) begin
-                     //if (cpha) begin
-                      //  nx_state   =  cpha_delay;
-                      //  dout_nxt = din;
-                     //end
-                    // else begin
-                       //if (cpol) nx_state   =  sample;
-                       //else      nx_state   =  drive;
-                       nx_state   =  sample;
-                       dout_nxt   =  {din[6:0],1'b0};;
-                      // din_nxt  = {data_in[6:0],mosi};
-                       
-                   //  end 
+                     if (cpha) begin
+                        nx_state   =  cpha_delay; 
+                     end
+                     else begin
+                       if (!cpol) nx_state   =  drive;
+                       else      nx_state   =  sample;
+                     end
+                     dout_nxt   = din;
                    end
                  end
       
       cpha_delay : begin 
-                      // if (cpol) nx_state   =  sample;
-                      // else      nx_state    =  drive;
-                     nx_state   =  sample;
-                     
+                       if (!cpol) nx_state   =  drive;
+                       else      nx_state    =  sample;
                    end
       
       drive      : begin // in next sclk cycle, capture mosi input into data_in reg
-                       
-                         
-                         nx_state = sample;
-                         
-                         dout_nxt = {dout_reg[6:0],1'b0};
+                       din_nxt  = {data_in[6:0],mosi};
+                       nx_state = sample;
                    end
         
       sample     : begin // in next sclk cycle, deliver miso output from dout reg, repeat till all DBITS are sent/captured
@@ -193,9 +187,9 @@ module spi_s (miso,dout,ready,sclk,mosi,done,din,cpha,cpol,rst);
                          dbits_cnt_nxt = '0;
                        end
                        else begin
-                         nx_state = drive;
-                         din_nxt  = {data_in[6:0],mosi};
                          dbits_cnt_nxt = dbits_cnt + 3'd1;
+                         dout_nxt = {dout_reg[6:0],1'b0};
+                         nx_state = drive;
                        end
                    end
      endcase
@@ -203,7 +197,7 @@ module spi_s (miso,dout,ready,sclk,mosi,done,din,cpha,cpol,rst);
  
   
   assign dout = (done) ? data_in : '0; // drive input reg contents at the end, should equal to 8 mosi bits received serially
-  assign miso = (dbits_cnt=='0 && pr_state!= drive) ? din[7] : dout_reg[7]; // drive miso output pin
+  assign miso = dout_reg[7]; // drive miso output pin
   
 endmodule
  
