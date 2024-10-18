22f58698910ed9eed27608e06d4028ae5b01133f
Tooba12322 <tuubaa1996@gmail.com>
2024-10-18 18:08:33 +0800

    Update cmd_if.sv

--- Interface_Design/cmd_if.sv
+++ Interface_Design/cmd_if.sv
@@ -1,36 +1,46 @@
-// Command interface design, using it inside a module as master and slave interface
-
 
 `timescale 1ns/1ps
 
-interface cmd_if #(parameter ADDRW = 32, BYTE_CNTw = 16) (input logic clk, logic rst);
+interface cmd_if #(parameter ADDRW = 32, parameter BYTE_CNTw = 16);
   
   logic req,wr;
   logic [BYTE_CNTw-1:0] byte_cnt;
   logic [ADDRW-1:0] start_addr;
   logic done,req_ack;
   
-  modport master (input clk,rst,done,req_ack, output req,byte_cnt,start_addr,wr);
-  modport slave (input req,byte_cnt,start_addr,wr,clk,rst, output done,req_ack);
+  modport master (input done,req_ack, output req,byte_cnt,start_addr,wr);
+  modport slave (input req,byte_cnt,start_addr,wr, output done,req_ack);
   
 endinterface
 
 
-module use_if #(parameter ADDRW = 32, BYTE_CNTw = 16) 
-    (cmd_if.master #(ADDRW , BYTE_CNTw) out ,
-     cmd_if.slave #(ADDRW , BYTE_CNTw) in);
-    
-  logic [16:0] byte_out,byte_out_nxt;
-  logic [16:0] byte_reg,byte_nxt;
+module use_if (clk, rst);
+  parameter ADDRW = 32;
+  parameter BYTE_CNTw = 16;
   
-  always @(posedge in.clk or negedge in.rst) begin
-    if (!in.rst) byte_reg     <= '0;
+  input clk,rst;
+  cmd_if out(.*); 
+  cmd_if in(.*); 
+  
+  logic [BYTE_CNTw-1:0] byte_out;
+  logic [BYTE_CNTw-1:0] byte_reg,byte_nxt;
+  logic [ADDRW-1:0] addr_reg,addr_nxt;
+  
+  
+  always @(posedge clk or negedge rst) begin
+    if (!rst) byte_reg     <= '0;
     else if (in.req) byte_reg <= in.byte_cnt;
     else byte_reg             <= byte_nxt;
   end
   
-  always @(posedge in.clk or negedge in.rst) begin
-    if (!in.rst)            byte_out <= '0;
+   always @(posedge clk or negedge rst) begin
+     if (!rst) addr_reg     <= '0;
+     else if (in.req) addr_reg <= in.start_addr;
+     else if (out.req) addr_reg <= addr_nxt;
+  end
+  
+  always @(posedge clk or negedge rst) begin
+    if (!rst)            byte_out <= '0;
     else if (byte_reg>=512) byte_out <= 16'd512;
     else if (byte_reg>=256) byte_out <= 16'd256;
     else if (byte_reg>=128) byte_out <= 16'd128;
@@ -39,20 +49,21 @@ module use_if #(parameter ADDRW = 32, BYTE_CNTw = 16)
     else                    byte_out <= byte_reg;
   end
   
-  always @(byte_out,byte_reg) begin
-    if      (byte_out[9] && byte_reg>=512) byte_nxt = byte_reg - 16'd512;
-    else if (byte_out[8] && byte_reg>=256) byte_nxt = byte_reg - 16'd256;
-    else if (byte_out[7] && byte_reg>=128) byte_nxt = byte_reg - 16'd128;
-    else if (byte_out[6] && byte_reg>=64)  byte_nxt = byte_reg - 16'd64;
-    else if (byte_out[5] && byte_reg>=32)  byte_nxt = byte_reg - 16'd32;
+  always @(byte_reg) begin
+    if      (byte_reg>=512) byte_nxt = byte_reg - 16'd512;
+    else if (byte_reg>=256) byte_nxt = byte_reg - 16'd256;
+    else if (byte_reg>=128) byte_nxt = byte_reg - 16'd128;
+    else if (byte_reg>=64)  byte_nxt = byte_reg - 16'd64;
+    else if (byte_reg>=32)  byte_nxt = byte_reg - 16'd32;
     else byte_nxt = '0;  
   end
   
+  assign addr_nxt       = (out.req) ? addr_reg + byte_out : '0;
   assign out.byte_cnt   = byte_out;
-  assign out.req        = (byte_reg!='0) ? '1 : '0;
+  assign out.req        = (byte_out!='0) ? '1 : '0;
   assign out.wr         = in.wr;
-  assign out.start_addr = in.start_addr;
-  assign in.reqack      = out.reqack;
+  assign out.start_addr = addr_reg;
+  assign in.req_ack     = out.req_ack;
   assign in.done        = (out.req && byte_reg=='0);
   
 endmodule
