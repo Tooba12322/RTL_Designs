c4cc122ab56780224878391fc2fa8ea1d3f2932f
Tooba12322 <tuubaa1996@gmail.com>
2024-10-19 16:06:53 +0800

    Update and rename AHB_master.sv to AHB_master/AHB_master.sv

---
+++ AHB_master/AHB_master.sv
@@ -0,0 +1,29 @@
+`timescale 1ns/1ps
+
+interface cmd_if #(parameter ADDRW = 32, parameter BYTE_CNTw = 16);
+  
+  logic req,wr;
+  logic [BYTE_CNTw-1:0] byte_cnt;
+  logic [ADDRW-1:0] start_addr;
+  logic done,req_ack;
+  
+  modport master (input done,req_ack, output req,byte_cnt,start_addr,wr);
+  modport slave (input req,byte_cnt,start_addr,wr, output done,req_ack);
+  
+endinterface
+
+interface ahb_if #(parameter ADDRW = 32, parameter DATAW = 32);
+  
+  logic req,wr;
+  logic [BYTE_CNTw-1:0] byte_cnt;
+  logic [ADDRW-1:0] start_addr;
+  logic done,req_ack;
+  
+  modport master (input done,req_ack, output req,byte_cnt,start_addr,wr);
+  modport slave (input req,byte_cnt,start_addr,wr, output done,req_ack);
+  
+endinterface
+
+module ahb_m();
+
+endmodule
