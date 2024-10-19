044e345194c52457b08354daa4588b72ca807d2c
Tooba12322 <tuubaa1996@gmail.com>
2024-10-19 16:41:13 +0800

    Update AHB_master.sv

--- AHB_master/AHB_master.sv
+++ AHB_master/AHB_master.sv
@@ -14,9 +14,9 @@ endinterface
 
 interface ahb_if #(parameter ADDRW = 32, parameter DATAW = 32);
   
-  logic req,wr;
-  logic [BYTE_CNTw-1:0] byte_cnt;
-  logic [ADDRW-1:0] start_addr;
+  logic hwrite;
+  logic [DATAW-1:0] hwdata, hrdata;
+  logic [ADDRW-1:0] haddr;
   logic done,req_ack;
   
   modport master (input done,req_ack, output req,byte_cnt,start_addr,wr);
