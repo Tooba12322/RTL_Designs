9a6f7b71f9a720fc2fe3b39ccae2f52bf5e6f807
Tooba12322 <tuubaa1996@gmail.com>
2026-04-11 18:56:46 +0530

    Update axi_s_mem_tb.sv

--- AXI_Slave/axi_s_mem_tb.sv
+++ AXI_Slave/axi_s_mem_tb.sv
@@ -1,5 +1,5 @@
 
-// Waveform : 
+// Waveform : https://www.edaplayground.com/x/AiCQ
 
 module axi_s_mem_tb ();
   
@@ -31,11 +31,11 @@ module axi_s_mem_tb ();
   
   axi_s_mem DUT(.*);
 
-  logic [31:0]  data;
-
+  logic [31:0]  wrdata, rddata;
+  logic [31:0] Rd_data;
   logic [7:0] addr;
   
-  task axi_write (input [7:0] addr, input [31:0] data);
+  task axi_write (input [7:0] addr, input [31:0] wrdata);
     begin
       // Drive write addr
       @(posedge aclk);
@@ -48,7 +48,7 @@ module axi_s_mem_tb ();
       // Drive write data
       @(posedge aclk);
       wvalid <= '1;
-      wdata  <= data;
+      wdata  <= wrdata;
       wait(wready);
       @(posedge aclk);
       wvalid <= '0;
@@ -63,11 +63,33 @@ module axi_s_mem_tb ();
     end
   endtask
   
+  task axi_read (input [7:0] addr, output [31:0] rddata);
+    begin
+      // Drive read addr
+      @(posedge aclk);
+      arvalid <= '1;
+      araddr  <= addr;
+      wait(arready);
+      @(posedge aclk);
+      arvalid <= '0;
+
+
+      // Check read response
+      @(posedge aclk);
+      rready <= '1;
+      wait(rvalid);
+      rddata <= rdata;
+      @(posedge aclk);
+      rready <= '0;
+
+    end
+  endtask
+  
   initial begin
     //generate waveform
     $dumpfile("axi_s_mem.vcd");
     $dumpvars(0,axi_s_mem_tb);
-    $monitor("End of write transaction, Data=%h, Addr=%h",DUT.mem.mem[15],awaddr);
+   // $monitor("End of write transaction, Data=%h, Addr=%h",DUT.mem.mem[15],awaddr);
 
     //Initialization
     aclk = '0;
@@ -81,15 +103,51 @@ module axi_s_mem_tb ();
     
     //Stimulus
     
-    axi_write(8'h0F, $urandom_range(32'h0A0E0E00,32'hCAFEBEDF));
-
-    $display("===========TEST PASSED============");
-    #80 $finish;
+    axi_write(8'h1F, $urandom_range(32'h0ABE0E54,32'hCAFEBEDF));
+    $display("=========== END OF WRITE TRANSACTION ============");
+    @(posedge aclk);
+    axi_read(8'h1F, Rd_data);
+    $display("=========== END OF READ TRANSACTION ============");
+    
+    if (Rd_data == DUT.mem.mem[31])
+      $display("=========== SUCCESS - DATA MATCH ============");
+    else
+      $display("============ FAILURE - DATA MIS-MATCH ===============");
+    
+    
+    axi_write(8'hBC, $urandom_range(32'h0A0C0E23,32'hCAFEABCD));
+    $display("===========END OF WRITE TRANSACTION============");
+    axi_read(8'hBC, Rd_data);
+    $display("=========== END OF READ TRANSACTION ============");
+    
+    if (Rd_data == DUT.mem.mem[188])
+      $display("=========== SUCCESS - DATA MATCH ============");
+    else
+      $display("============ FAILURE - DATA MIS-MATCH ===============");
+    
+    $display("==============TEST PASSED==============");
+    #10 $finish;
   end
   
    //Clk Generation 
   always #2 aclk = !aclk;
   
 endmodule
+
+Output :
+VCD info: dumpfile axi_s_mem.vcd opened for output.
+==================================================
+=========AXI Simulation Started==========
+==================================================
+=========== END OF WRITE TRANSACTION ============
+=========== END OF READ TRANSACTION ============
+=========== SUCCESS - DATA MATCH ============
+===========END OF WRITE TRANSACTION============
+=========== END OF READ TRANSACTION ============
+=========== SUCCESS - DATA MATCH ============
+==============TEST PASSED==============
+testbench.sv:129: $finish called at 112 (1s)
+Finding VCD file...
+./axi_s_mem.vcd
 
              
